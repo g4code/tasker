ad823a2422699643f1dc3e1ee8f89d5b6c1f9f64
Dejan Samardzija <samardzija.dejan+hq@gmail.com>
2014-01-11 18:14:45 +0100

    removed priority constants

--- src/G4/Tasker/Consts.php
+++ src/G4/Tasker/Consts.php
@@ -9,9 +9,5 @@ class Consts
     const STATUS_BROKEN  = 3;
     const STATUS_DONE    = 2;
 
-    const PRIORITY_LOW    = 1;
-    const PRIORITY_MEDIUM = 11;
-    const PRIORITY_HIGH   = 21;
-
     const LIMIT_DEFAULT = 100;
 }
