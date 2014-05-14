373eaa60cf9fa98bc8e3ff6c74848846e0d4d6ac
Ivan Krickovic <ivankoni@gmail.com>
2014-05-14 22:20:13 +0200

    Consts class expanded with new constants and methods

--- src/Consts.php
+++ src/Consts.php
@@ -14,4 +14,55 @@ class Consts
     const PRIORITY_HIGH   = 81;
 
     const LIMIT_DEFAULT = 100;
+
+    const STATUS_NAME_PENDING = 'STATUS_PENDING';
+    const STATUS_NAME_WORKING = 'STATUS_WORKING';
+    const STATUS_NAME_BROKEN  = 'STATUS_BROKEN';
+    const STATUS_NAME_DONE    = 'STATUS_DONE';
+
+    const ORDER_BY_CREATED_ON = 'created_ts';
+    const ORDER_BY_PRIORITY   = 'priority';
+
+    const ORDER_BY_NAME_DEFAULT    = self::ORDER_BY_NAME_CREATED_ON;
+    const ORDER_BY_NAME_CREATED_ON = 'CREATED_ON';
+    const ORDER_BY_NAME_PRIORITY   = 'PRIORITY';
+
+    public static function getMap()
+    {
+        return array(
+            'STATUS_PENDING' => self::STATUS_PENDING,
+            'STATUS_WORKING' => self::STATUS_WORKING,
+            'STATUS_BROKEN'  => self::STATUS_BROKEN,
+            'STATUS_DONE'    => self::STATUS_DONE,
+            'CREATED_ON'     => self::ORDER_BY_CREATED_ON,
+            'PRIORITY'       => self::ORDER_BY_PRIORITY,
+        );
+    }
+
+    public static function getConst($constName)
+    {
+        $map = static::getMap();
+        return static::isValid($constName)
+            ? $map[$constName]
+            : null;
+    }
+
+    public static function getName($const)
+    {
+        return array_search($const, static::getMap());
+    }
+
+    public static function getValidOrderBy()
+    {
+        return array(
+            self::ORDER_BY_NAME_CREATED_ON,
+            self::ORDER_BY_NAME_PRIORITY,
+        );
+    }
+
+    public static function isValid($constName)
+    {
+        $map = static::getMap();
+        return isset($map[$constName]);
+    }
 }
