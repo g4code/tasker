90b18387529374445e5f5c2083d33eaad6acba1a
Sanja Medic <sanjamedic86@gmail.com>
2017-04-07 10:42:43 +0200

    Add new status: COMPLETED_NOT_DONE

--- src/Consts.php
+++ src/Consts.php
@@ -4,12 +4,13 @@ namespace G4\Tasker;
 
 class Consts
 {
-    const STATUS_PENDING           = 0;
-    const STATUS_MULTI_WORKING     = 5;
-    const STATUS_WORKING           = 1;
-    const STATUS_BROKEN            = 3;
-    const STATUS_RETRY_FAILED      = 4;
-    const STATUS_DONE              = 2;
+    const STATUS_PENDING            = 0;
+    const STATUS_MULTI_WORKING      = 5;
+    const STATUS_WORKING            = 1;
+    const STATUS_BROKEN             = 3;
+    const STATUS_RETRY_FAILED       = 4;
+    const STATUS_DONE               = 2;
+    const STATUS_COMPLETED_NOT_DONE = 6;
 
     const PRIORITY_LOW             = 11;
     const PRIORITY_MEDIUM          = 41;
@@ -33,12 +34,13 @@ class Consts
 
     const LIMIT_DEFAULT            = 100;
 
-    const STATUS_NAME_PENDING       = 'STATUS_PENDING';
-    const STATUS_NAME_MULTI_WORKING = 'STATUS_MULTI_WORKING';
-    const STATUS_NAME_WORKING       = 'STATUS_WORKING';
-    const STATUS_NAME_BROKEN        = 'STATUS_BROKEN';
-    const STATUS_NAME_RETRY_FAILED  = 'STATUS_RETRY_FAILED';
-    const STATUS_NAME_DONE          = 'STATUS_DONE';
+    const STATUS_NAME_PENDING            = 'STATUS_PENDING';
+    const STATUS_NAME_MULTI_WORKING      = 'STATUS_MULTI_WORKING';
+    const STATUS_NAME_WORKING            = 'STATUS_WORKING';
+    const STATUS_NAME_BROKEN             = 'STATUS_BROKEN';
+    const STATUS_NAME_RETRY_FAILED       = 'STATUS_RETRY_FAILED';
+    const STATUS_NAME_DONE               = 'STATUS_DONE';
+    const STATUS_NAME_COMPLETED_NOT_DONE = 'STATUS_COMPLETED_NOT_DONE';
 
     const ORDER_BY_CREATED_ON      = 'ts_created';
     const ORDER_BY_PRIORITY        = 'priority';
@@ -50,14 +52,15 @@ class Consts
     public static function getMap()
     {
         return array(
-            self::STATUS_NAME_PENDING       => self::STATUS_PENDING,
-            self::STATUS_NAME_MULTI_WORKING => self::STATUS_MULTI_WORKING,
-            self::STATUS_NAME_WORKING       => self::STATUS_WORKING,
-            self::STATUS_NAME_BROKEN        => self::STATUS_BROKEN,
-            self::STATUS_NAME_RETRY_FAILED  => self::STATUS_RETRY_FAILED,
-            self::STATUS_NAME_DONE          => self::STATUS_DONE,
-            self::ORDER_BY_NAME_CREATED_ON  => self::ORDER_BY_CREATED_ON,
-            self::ORDER_BY_NAME_PRIORITY    => self::ORDER_BY_PRIORITY,
+            self::STATUS_NAME_PENDING            => self::STATUS_PENDING,
+            self::STATUS_NAME_MULTI_WORKING      => self::STATUS_MULTI_WORKING,
+            self::STATUS_NAME_WORKING            => self::STATUS_WORKING,
+            self::STATUS_NAME_BROKEN             => self::STATUS_BROKEN,
+            self::STATUS_NAME_RETRY_FAILED       => self::STATUS_RETRY_FAILED,
+            self::STATUS_NAME_DONE               => self::STATUS_DONE,
+            self::STATUS_NAME_COMPLETED_NOT_DONE => self::STATUS_COMPLETED_NOT_DONE,
+            self::ORDER_BY_NAME_CREATED_ON       => self::ORDER_BY_CREATED_ON,
+            self::ORDER_BY_NAME_PRIORITY         => self::ORDER_BY_PRIORITY,
         );
     }
 
