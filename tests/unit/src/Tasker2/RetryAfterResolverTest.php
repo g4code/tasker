9e6977e4d2124e602832a35af02200729e4e16b2
mirko bukilic <mirko.bukilic@codeplicity.com>
2021-12-02 19:43:41 +0100

    Added option to pass arbitrary parameters for RetryAfterResolver - tests updated

--- tests/unit/src/Tasker2/RetryAfterResolverTest.php
+++ tests/unit/src/Tasker2/RetryAfterResolverTest.php
@@ -14,22 +14,34 @@ class RetryAfterResolverTest extends \PHPUnit_Framework_TestCase
     private $resolverDelay;
 
     /**
+     * @var \G4\Tasker\Tasker2\RetryAfterResolver
+     */
+    private $resolverDelayEmptyArray;
+
+    /**
+     * @var \G4\Tasker\Tasker2\RetryAfterResolver
+     */
+    private $resolverDelayNotSet;
+
+    /**
      * Set up method
      */
     protected function setUp()
     {
         $this->delayForRetries = [
-            1 => 10,
-            2 => 10,
-            3 => 20,
-            4 => 60,
-            5 => 120,
-            6 => 600,
-            7 => 1800,
-            8 => 86400
+            10,
+            10,
+            20,
+            60,
+            120,
+            600,
+            1800,
+            86400
         ];
 
         $this->resolverDelay = new \G4\Tasker\Tasker2\RetryAfterResolver($this->delayForRetries);
+        $this->resolverDelayEmptyArray = new \G4\Tasker\Tasker2\RetryAfterResolver([]);
+        $this->resolverDelayNotSet = new \G4\Tasker\Tasker2\RetryAfterResolver();
     }
 
     /**
@@ -39,6 +51,8 @@ class RetryAfterResolverTest extends \PHPUnit_Framework_TestCase
     {
         $this->delayForRetries = null;
         $this->resolverDelay = null;
+        $this->resolverDelayNotSet = null;
+        $this->resolverDelayEmptyArray = null;
     }
 
     /**
@@ -75,7 +89,61 @@ class RetryAfterResolverTest extends \PHPUnit_Framework_TestCase
 
     public function testGetDelayForRetries()
     {
-        $this->assertSame($this->delayForRetries, $this->resolverDelay->getDelayForRetries());
+        $testArray = [
+            1 => 10,
+            2 => 10,
+            3 => 20,
+            4 => 60,
+            5 => 120,
+            6 => 600,
+            7 => 1800,
+            8 => 86400
+        ];
+        $this->assertSame($testArray, $this->resolverDelay->getDelayForRetries());
+    }
+
+    public function testDelayForRetriesNotSet()
+    {
+        $testArray = [
+            1 => 60,
+            2 => 300,
+            3 => 1000
+        ];
+
+        $this->assertSame($testArray, $this->resolverDelayNotSet->getDelayForRetries());
+    }
+
+    public function testMaxCountDelayForRetriesNotSet()
+    {
+        $defaultDelay = [
+            60,
+            300,
+            1000
+        ];
+
+        $this->assertSame(count($defaultDelay), $this->resolverDelayNotSet->getMaxRetryAttempts());
+    }
+
+    public function testDelayForRetriesEmptyArray()
+    {
+        $testArray = [
+            1 => 60,
+            2 => 300,
+            3 => 1000
+        ];
+
+        $this->assertSame($testArray, $this->resolverDelayEmptyArray->getDelayForRetries());
+    }
+
+    public function testMaxCountDelayForRetriesEmptyArray()
+    {
+        $defaultDelay = [
+            60,
+            300,
+            1000
+        ];
+
+        $this->assertSame(count($defaultDelay), $this->resolverDelayEmptyArray->getMaxRetryAttempts());
     }
 
     public function resolverDelayForRetriesDataProvider()
