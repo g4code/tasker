fb40960264faeb2fc46398328b5c005ee5a225f9
Petar Pavlovic <pavlovicpetar@gmail.com>
2023-07-11 10:47:53 +0200

    Added a caching to DiscoveryService (#41)

--- src/Model/Domain/TaskerPool/TaskerPoolRepository.php
+++ src/Model/Domain/TaskerPool/TaskerPoolRepository.php
@@ -4,10 +4,14 @@ namespace G4\Tasker\Model\Domain\TaskerPool;
 
 class TaskerPoolRepository
 {
+    use CachingTrait;
+
     const TABLE_NAME = 'tasker_pool';
     const INACTIVE = 0;
     const ACTIVE = 1;
     const HOST_ALIVE_SECONDS = 90;
+
+    const CACHE_KEY = 'tasker_hostnames';
 
     /**
      * @var \PDO
@@ -18,13 +22,27 @@ class TaskerPoolRepository
      */
     private $hostAvailabilityTime;
 
+    /**
+     * @var \G4\Mcache\Mcache
+     */
+    private $cache;
+
     public function __construct(\PDO $pdo, $hostAvailabilityTime = self::HOST_ALIVE_SECONDS)
     {
         $this->pdo = $pdo;
         $this->hostAvailabilityTime = $hostAvailabilityTime;
     }
 
-    public function upsert(string $hostname)
+    public function setCache(\G4\Mcache\Mcache $cache)
+    {
+        $this->cache = $cache;
+        return $this;
+    }
+
+    /**
+     * @param string $hostname
+     */
+    public function upsert($hostname)
     {
         $query = sprintf(
             "INSERT INTO %s (`hostname`,`status`,`ts_available`) VALUES ('%s',%s,%s) ON DUPLICATE KEY UPDATE ts_available=%s;",
@@ -48,10 +66,15 @@ class TaskerPoolRepository
     }
 
     /**
-     * @return array|null[]
+     * @return array|null
      */
     public function getAvailableHostnames()
     {
+        $cachedHostnames = $this->fetchFromCache();
+        if ($cachedHostnames) {
+            return $cachedHostnames;
+        }
+
         $query = sprintf(
             "SELECT (hostname) FROM %s WHERE status = %s AND ts_available >= %s;",
             self::TABLE_NAME,
@@ -66,6 +89,8 @@ class TaskerPoolRepository
             return isset($data['hostname']) ? $data['hostname'] : null;
         }, $stmt->fetchAll());
 
-        return array_filter($hostsArray);
+        $hostnames = array_filter($hostsArray);
+        $this->setToCache($hostnames);
+        return $hostnames;
     }
 }
